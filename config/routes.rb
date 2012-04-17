f3c0ba0635ed96aebe187989644cfedfeb27efb1
Dominic Bou-Samra <domlebo70@Dominics-MacBook-Air.local>
2012-04-18 08:36:12 +1000

    Done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 SampleApp::Application.routes.draw do
+  get "pages/about"
+
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
